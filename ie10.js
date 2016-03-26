c63c99e0db3851e549160939aaf2a49a71e542cb
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-03-27 00:50:29 +0300

    ie10.js: dataset supported for IE9-10

---
+++ ie10.js
@@ -0,0 +1,58 @@
+(function () {
+	"use strict";
+
+	if (!Element.dataset && !Element.prototype.dataset) {
+
+		Object.defineProperty (Element.prototype, 'dataset', {
+			get: function () {
+				var data = {},
+				    attrs = this.attributes,
+				    self = this;
+
+				for (var i = 0, l = attrs.length; i < l; ++i) {
+					var attr = attrs[i],
+					    name = attr.name;
+
+					if (!name || !/^data\-.*/.test(name))
+						continue;
+
+					(function (name, value) { 
+						Object.defineProperty(data, name.substr(name.indexOf('-') + 1), {
+							get: function () { return self.getAttribute(name); },
+							set: function (v) {
+								self.setAttribute(name, v);
+							},
+							enumerable: true,
+							configurable: false
+						});
+					})(name, attr.value)
+
+				}
+
+				return data;
+			},
+			set: function (data) {
+
+				if (typeof data !== 'object')
+					throw "Incorrect type of data set, must be an Object";
+
+				var attrs = this.attributes;
+
+				for (var i = 0; i < attrs.length; ++i) {
+					var name = attrs[i].name;
+
+					if (!name || !/^data\-.*/.test(name))
+						continue;
+
+					this.removeAttribute(name);
+					--i;
+				}
+
+				for (var key in data)
+					this.setAttribute(['data', key].join('-'), data[key]);
+			},
+			enumerable: true,
+			configurable: false
+		});
+	}
+})()
